243f12bbd7af54af1e6b40e336d8160ea5866cdf
Eze Sunday Eze <mailsforeze@gmail.com>
2022-01-22 09:09:21 +0100

    fix the docs and some refactoring

--- index.sol
+++ index.sol
@@ -6,8 +6,8 @@ import "./chainlink-price-aggregator.sol";
 contract Prediction {
     mapping(address => uint256) public participants;
     mapping(address => uint256) public winners;
-    mapping(address => int256) public bids;
-    uint256 public totalBidsAmount;
+    mapping(address => int256) public predictions;
+    uint256 public totalPredictionAmount;
     uint256 public totalParticipants;
     address public owner;
     uint256 public startTime;
@@ -15,67 +15,71 @@ contract Prediction {
     int256 public openingValue;
     int256 public closingValue;
     string public coin;
-    int256 public bidAmount;
+    int256 public predictionAmount;
     AggregatorV3Interface internal priceFeed;
 
-    event DepositEvent(uint256 _amount, address _participant);
-
-    constructor(int256 _bidAmount) {
+    constructor() {
         owner = msg.sender;
         coin = "ETH/USD";
-        bidAmount = _bidAmount;
         priceFeed = AggregatorV3Interface(
             0x9326BFA02ADD2366b30bacB125260Af641031331
         );
     }
 
+    event DepositEvent(uint256 _amount, address _participant);
+
     receive() external payable {
         emit DepositEvent(msg.value, msg.sender);
     }
 
-    function bid(int256 _price) external payable {
+    // @dev: This function is used to set a participant's price  prediction and to make the default contribution by all participants.
+    function predict(int256 _price) external payable {
         if (block.timestamp > endTime) {
-            revert("Bidding is closed");
+            revert("Prediction closed");
         }
 
-        if (int256(msg.value) == bidAmount) {
-            revert("Invalid bid");
+        if (int256(msg.value) == predictionAmount) {
+            revert("Invalid prediction amount");
         }
 
         participants[msg.sender] = msg.value;
-        bids[msg.sender] = _price;
-        totalBidsAmount = totalBidsAmount + msg.value;
+        predictions[msg.sender] = _price;
+        totalPredictionAmount = totalPredictionAmount + msg.value;
         totalParticipants++;
     }
-
-    function start() public {
+    // Start a new prediction circle
+    function startPredictionCircle(int256 _predictionAmount) public {
         startTime = block.timestamp;
+        predictionAmount = _predictionAmount;
+        totalPredictionAmount = 0;
+        totalParticipants = 0;
         openingValue = getLatestPrice();
     }
-
-    function end() public {
+    // end the prediction circle
+    function endPredictionCirle() public {
         closingValue = getLatestPrice();
         endTime = block.timestamp;
     }
 
+    // a Participant can withdraw his deposit and profit if the prediction is in their advantage
     function checkStatusAndWithdraw(address payable _participant)
         public
         payable
     {
         if (block.timestamp < endTime) {
-            revert("Bidding is not closed yet");
+            revert("prediction is not closed yet");
         }
         require(
-            int256(participants[_participant]) != (bidAmount),
+            int256(participants[_participant]) != (predictionAmount),
             "Not allowed"
         );
-        if (closingValue >=bids[_participant] ) {
-            // transfer equal amount of totalBidsamount to all winners
-            uint256 winningAmount = totalBidsAmount / totalParticipants;
+        if (closingValue >= predictions[_participant]) {
+            // transfer equal amount of totalPredictionAmount to all winners
+            uint256 winningAmount = totalPredictionAmount / totalParticipants;
             _participant.transfer(winningAmount);
         }
     }
-
+    // @dev: This function is used to get the latest price from the offchain price feed
     function getLatestPrice() public view returns (int256) {
         (
             uint80 roundID,
--- readme.md
+++ readme.md
@@ -1,14 +1,19 @@
 # Crypto Gambling Smart Contract
 
-The idea is that if you deploy this smart contract, participants can bet on the price of Ethereum
-If the price goes higher as of the time the bet was closed -- betting should ideally start in the morning and stop in the evening but since it's flexible, anytime that's specified is will work.
+The idea is that if you deploy this smart contract, participants can bet on the price of Ethereum, whether it will go up or down.
+If the price goes up those who said it will go up from the opening price will become winners while those who predict it will go down will lose.
+Each participant will set a price they believe it will get to and go above.
+
 - We use chainlink to get Ethereum current price
-- Every participant can withraw their funds with their initial investment. If all participants make predict correctly they will be nothing to benefit as participants benefits from others wrong guess.
-- If participant predicts wrongly, they lose everything.
+- Every participant can withraw their funds with their initial investment. If all participants make predict correctly they will be nothing to benefit as participants benefit from others wrong guesses.
+- If participants predicts wrongly, they lose everything.
 - It's gambling. It's not business.
 
-This is a work in progress. 
 
 ## Short comings
 - It's currently dividing the profit by all the participants instead of only the participants that guess right, leaving some funds that no one gets to claim.
-- Need to be able to get the total number of participants who were successful and those who lose without loops.
+- Need to be able to get the total number of participants who were successful and those who lose without using loops.
+
+## *Disclaimer*: 
+Cryptocurrency transactions comes with risks. Sometimes, funds can get lost and are unrecoverable. If you are going to try this or any other contract make sure you test it on the testnet sufficiently. 
+I am not responsible for any loss of funds. This is for educational/entertainment purpose only.
